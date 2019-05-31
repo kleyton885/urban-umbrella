c8fd46020dea0c97ddda58fb0b675c9b4b197f91
kleyton885 <51056018+kleyton885@users.noreply.github.com>
2019-05-31 11:16:15 -0300

    Update moverPersonagem.cs

--- moverPersonagem.cs
+++ moverPersonagem.cs
@@ -10,23 +10,43 @@ public class moverPersonagem : MonoBehaviour
 	float velocidade = 5.0f;
 	private float pulo = 5.0f;
 	
+	public GameObject jogador;
+	public Animation animacao;
+	
     void Start()
     {
         objetoCharControler = GetComponent<CharacterController>();
+		animacao = jogador.GetComponent<Animation>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 forward = Input.GetAxi("Vertical") * transform.TransformDirection(Vector3.forward) * velocidade;
+        Vector3 forward = Input.GetAxis("Vertical") * transform.TransformDirection(Vector3.forward) * velocidade;
 		transform.Rotate(new Vector3(0,Input.GetAxis("Horizontal") * giro *Time.deltaTime,0));
 		
 		objetoCharControler.Move(forward * Time.deltaTime);
 		objetoCharControler.SimpleMove(Physics.gravity);
 		
 		if (Input.GetButton("Jump")){
-			if (objetoCharControler.isGrounded == true) { vetorDirecao.y = pulo; }
+			if (objetoCharControler.isGrounded == true){
+				vetorDirecao.y = pulo;
+				jogador.GetComponent<Animation>().Play("pulando");
+			}
+		}else{
+			if (Input.GetButton("Horizontal") || Input.GetButton("Vertical") ){
+				if (!animacao.IsPlaying("pulando")){
+					jogador.GetComponent<Animation>().Play("andando");
+				}
+			}else{
+				if (objetoCharControler.isGrounded == true){
+					jogador.GetComponent<Animation>().Play("parado");
+				}
+			}
 		}
+		
+		
+		
 		vetorDirecao.y -= 3.0f * Time.deltaTime;
 		objetoCharControler.Move(vetorDirecao * Time.deltaTime);
     }
